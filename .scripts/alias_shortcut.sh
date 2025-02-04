3a6751b20d1324a5d3ea8b61f97cc6152424036f
Sahil <sahilmakhamale88@gmail.com>
2025-02-04 10:54:57 +0530

    done

---
+++ .scripts/alias_shortcut.sh
@@ -0,0 +1,11 @@
+alias scripts='cd ~/.scripts/'
+alias mz='micro ~/.zshrc'
+alias sz='source ~/.zshrc'
+alias fs="yazi"
+alias codei="code-insiders"
+alias clrr="history -c"
+
+
+# brew install fzf fd  bat git-delta eza tldr zoxide
+
+# git clone https://github.com/junegunn/fzf-git.sh.git
