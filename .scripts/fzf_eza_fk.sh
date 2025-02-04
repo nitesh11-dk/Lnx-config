77ae34328ee30b853297dafe463a3493d3e7e5c8
Niteshkumar Kushwaha <nittesh.dk@gmail.com>
2025-02-04 11:32:38 +0530

    Update fzf_eza_fk.sh

--- .scripts/fzf_eza_fk.sh
+++ .scripts/fzf_eza_fk.sh
@@ -24,9 +24,6 @@ _fzf_compgen_dir() {
 }
 
 
-source ~/.fzf-git.sh/fzf-git.sh
-
-
 show_file_or_dir_preview="if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi"
 
 export FZF_CTRL_T_OPTS="--preview '$show_file_or_dir_preview'"
